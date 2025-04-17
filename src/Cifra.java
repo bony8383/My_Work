7f9b49a26128d4eb17412e84f7fc1f2ff096f180
bony8383 <88745088+bony8383@users.noreply.github.com>
2025-04-17 22:52:56 +0200

    second commit

---
+++ src/Cifra.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Cifra {
+    //Колдонуучу киргизген эки санды бири-бирине алмаштыр.
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Insert your first number:");
+        int a = sc.nextInt();
+        System.out.println("Insert second  number:");
+        int b = sc.nextInt();
+        int c = 0;
+         c = a;
+         a = b;
+         b = c;
+        System.out.println("After swapping:");
+        System.out.println("First number: " + a);
+        System.out.println("Second number: " + b);
+
+
+
+       }
+
+    }
+
